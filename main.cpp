8d2af89b89c9f2fe4ebf1b12f1ef39872e6f0927
SirImperivm <riccardo.chimento24@gmail.com>
2025-04-25 12:17:44 +0200

    Add a dice game implementation with player-computer rounds
    Replaced template code with a game where the player competes against the computer through multiple rounds. The player inputs a number, and dice rolls determine the winner for each round. The game tracks and displays points, then announces the overall winner at the end.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,55 @@
 #include <iostream>
+#include <random>
+#include <time.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    int playerPoints = 0;
+    int computerPoints = 0;
+
+    int maxRounds = 6;
+    int round = 1;
+
+    while (round < maxRounds) {
+        srand(time(NULL));
+        int value;
+
+        cout << "Round numero " << round << endl;
+        cout << "Inserisci un valore tra 6 e 12: ";
+        cin >> value;
+
+        while (value < 6 || value > 12) {
+            cout << "Valore non valido, inseriscine uno tra 6 e 12: ";
+            cin >> value;
+        }
+
+        int dice1 = rand() % 6 + 1;
+        int dice2 = rand() % 6 + 1;
+
+        int computerValue = dice1 + dice2;
+
+        if (computerValue >= value) {
+            cout << "Il computer ha scelto " << computerValue << " questo round è stato vinto da lui.";
+            computerPoints++;
+        } else {
+            cout << "Il computer ha scelto " << computerValue << " questo round hai vinto te.";
+            playerPoints++;
+        }
+
+        cout << endl;
+
+        cout << "Punti Attuali: " << endl;
+        cout << "Player: " << playerPoints << endl;
+        cout << "Computer: " << computerPoints << endl;
+
+        round++;
     }
 
+    cout << "Fine del gioco." << endl;
+    if (playerPoints > computerPoints) cout << "Hai vinto!";
+    else if (playerPoints < computerPoints) cout << "Il computer ha vinto!";
+    else cout << "Avete pareggato!";
+
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
